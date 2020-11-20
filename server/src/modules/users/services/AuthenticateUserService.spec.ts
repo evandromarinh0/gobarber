78fc64ccfb25abf429141ee73d1e3ca7bd15a69f
Evandro Marinho <evanforgotlastone@hotmail.com>
2020-11-20 15:03:39 -0300

    backend fully complete

---
+++ server/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -0,0 +1,69 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
+import AuthenticateUserService from './AuthenticateUserService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
+let authenticateUser: AuthenticateUserService;
+
+describe('AuthenticateUser', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashProvider = new FakeHashProvider();
+
+    authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+  });
+
+  it('should be able to authenticate', async () => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashProvider = new FakeHashProvider();
+
+    authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: '123456',
+    });
+
+    const authenticatedUser = await authenticateUser.execute({
+      email: 'johndoe@gmail.com',
+      password: '123456',
+    });
+
+    expect(authenticatedUser).toHaveProperty('token');
+    expect(authenticatedUser.user).toEqual(user);
+  });
+
+  it('should not be able to authenticate with non existing user', async () => {
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@gmail.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to authenticate with wrong password', async () => {
+    await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: '123456',
+    });
+
+    await expect(
+      authenticateUser.execute({
+        email: 'johndoe@gmail.com',
+        password: 'wrongpassword',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
---
+++ server/src/shared/container/providers/index.ts
@@ -0,0 +1,33 @@
+import { container } from 'tsyringe';
+
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
+import DiskStorageProvider from '@shared/container/providers/StorageProvider/implementations/DiskStorageProvider';
+
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import EtherealMailProvider from '@shared/container/providers/MailProvider/implementations/EtherealMailProvider';
+
+import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+
+import iCacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import RedisCacheProvider from '@shared/container/providers/CacheProvider/implementations/RedisCacheProvider';
+
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  DiskStorageProvider,
+);
+
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandlebarsMailTemplateProvider,
+);
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  container.resolve(EtherealMailProvider),
+);
+
+container.registerSingleton<iCacheProvider>(
+  'CacheProvider',
+  RedisCacheProvider,
+);
