3c3346a128445eec5e48e95d00673fe0ff60de55
Evandro Marinho <evanforgotlastone@hotmail.com>
2020-12-02 11:19:50 -0300

    link dashboard/profile created

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -3,6 +3,7 @@ import {  isToday, format, parseISO, isAfter} from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { FiClock, FiPower } from 'react-icons/fi';
 import DayPicker, { DayModifiers } from 'react-day-picker';
+import { Link } from 'react-router-dom';
 import 'react-day-picker/lib/style.css';
 
 import api from '../../services/api';
@@ -133,11 +134,13 @@ const Dashboard: React.FC = () => {
         <HeaderContent>
           <img src={logoImg} alt="GoBarber"/>
           <Profile>
-            <img src={user.avatar_url} alt={user.name} />
-            <div>
-              <span>Bem-vindo,</span>
-              <strong>{user.name}</strong>
-            </div>
+              <img src={user.avatar_url} alt={user.name} />
+              <div>
+                <span>Bem-vindo,</span>
+                <Link to="/profile">
+                  <strong>{user.name}</strong>
+                </Link>
+              </div>
           </Profile>
 
           <button onClick={signOut} type="button">
