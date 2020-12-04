60d935ed56f4fb3704ad4c524ff2d96b364cd789
Evandro Marinho <evanforgotlastone@hotmail.com>
2020-12-04 11:12:24 -0300

    signin styles

--- appgobarber/src/pages/SignIn/styles.ts
+++ appgobarber/src/pages/SignIn/styles.ts
@@ -1,5 +1,14 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  margin: 64px 0 24px;
+  color: #f4ede8;
+  font-size: 24px;
+  font-family: 'RobotoSlab-Medium';
 `;
