fe7c4ee3984895240179eb2fefb46091adcc3f8a
Evandro Marinho <evanforgotlastone@hotmail.com>
2020-12-07 23:38:19 -0300

    signin options added

--- appgobarber/src/pages/SignIn/index.tsx
+++ appgobarber/src/pages/SignIn/index.tsx
@@ -1,25 +1,57 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import logoImg from '../../assets/logo.png';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
-import { Container, Title } from './styles';
+import { Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText  
+} from './styles';
 
 const SignIn: React.FC = () => {
   return(
-    <Container>
-      <Image source={logoImg} />
-      <Title>Faça seu login</Title>
+    <>
+    <KeyboardAvoidingView 
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
+    >
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <Container>
+          <Image source={logoImg} />
+          <View>
+            <Title>Faça seu login</Title>
+          </View>
 
-      <Input name="name" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
+          <Input name="name" icon="mail" placeholder="E-mail" />
+          <Input name="password" icon="lock" placeholder="Senha" />
 
-      <Button>Entrar</Button>
+          <Button>Entrar</Button>
 
-    </Container>
+          <ForgotPassword>
+            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+          </ForgotPassword>
+        </Container>
+
+        <CreateAccountButton>
+          <Icon name="log-in" size={20} color="#ff9000" />
+          <CreateAccountButtonText>
+            Criar uma conta
+          </CreateAccountButtonText>
+        </CreateAccountButton>
+      </ScrollView>
+    </KeyboardAvoidingView>
+    </>
   );
 }
 
