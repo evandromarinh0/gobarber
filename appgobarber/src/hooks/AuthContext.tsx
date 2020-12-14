c4208a6a6d442fdb25e1f92d1466e6fec6507be8
Evandro Marinho <evanforgotlastone@hotmail.com>
2020-12-14 17:15:02 -0300

    context api connected

---
+++ appgobarber/src/hooks/AuthContext.tsx
@@ -0,0 +1,82 @@
+import React, { createContext, useCallback, useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import api from '../services/api';
+
+interface User {
+  id: string;
+  avatar_url: string;
+  email: string;
+  name: string;
+}
+
+interface AuthState {
+  token: string;
+  user: User;
+}
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  user: User;
+  signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
+  // updateAvatar(user: User): void;
+}
+
+export const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>({} as AuthState);
+
+  useEffect(() => {
+    async function loadStoragedData(): Promise<void> {
+      const token = await AsyncStorage.getItem('@GoBarber:token');
+      const user = await AsyncStorage.getItem('@GoBarber:user');
+
+      if (token && user) {
+        setData({ token, user: JSON.parse(user) })
+      }
+    }
+    loadStoragedData();
+  }, []);
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('sessions', {
+      email,
+      password,
+    });
+    const { token, user } = response.data;
+    await AsyncStorage.setItem('@Gobarber:token', token);
+    await AsyncStorage.setItem('@Gobarber:user', JSON.stringify(user));
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
+
+    setData({ token, user })
+  }, []);
+
+  const signOut = useCallback(async () => {
+    await AsyncStorage.removeItem('@Gobarber:token');
+    await AsyncStorage.removeItem('@Gobarber:user');
+
+    setData({} as AuthState);
+  }, []);
+
+  // const updateAvatar = useCallback((user: User) => {
+    
+  //   localStorage.setItem('@Gobarber:user', JSON.stringify(user));
+
+  //   setData({
+  //     token: data.token,
+  //     user,
+  //   })
+  // }, [data.token]);
+
+  return(
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
