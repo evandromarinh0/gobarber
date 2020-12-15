8dd2378cdf34f15d66b603c43dcc329fc34223d0
Evandro Marinho <evanforgotlastone@hotmail.com>
2020-12-15 11:35:54 -0300

    dashboard page created

---
+++ appgobarber/src/pages/Dashboard/index.tsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import { View } from 'react-native';
+
+import { Container } from './styles';
+
+const Dashboard: React.FC = () => {
+
+  return(
+    <Container>
+      <View />
+    </Container>
+  );
+}
+
+export default Dashboard;
