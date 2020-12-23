e096187d2db40f0bafd98acd07638d407bb91d3d
Evandro Marinho <evanforgotlastone@hotmail.com>
2020-12-23 11:39:40 -0300

    appointmentcreated route added

--- appgobarber/src/routes/app.routes.tsx
+++ appgobarber/src/routes/app.routes.tsx
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
 import CreateAppointment from '../pages/CreateAppointment';
+import AppointmentCreated from '../pages/AppointmentCreated';
 
 const App = createStackNavigator();
 
@@ -13,6 +14,7 @@ const AppRoutes: React.FC = () => {
       <App.Screen name="Dashboard" component={Dashboard} />
       <App.Screen name="Profile" component={Profile} />
       <App.Screen name="CreateAppointment" component={CreateAppointment} />
+      <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
     </App.Navigator>
   );
 }
